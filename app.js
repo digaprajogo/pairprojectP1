c0055b4809e7e540e6d4afe4cda75fc5b0a1b199
Diga Prajogo <prajogodiga@gmail.com>
2022-02-25 05:17:27 +0700

    coba deploy 5

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const routers = require('./routes/index')
 const Controller = require('./controller/controller')
-const port = 3000
+const port = process.env.PORT || 3000;
 const session = require('express-session')
+const { eventNames } = require('geolocation')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
